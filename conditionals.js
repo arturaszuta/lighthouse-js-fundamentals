b5c8d11bf984cf1fffedf43f55ba4fd81c01834a
Arturas Zuta <arturaszuta@gmail.com>
2019-06-13 19:00:29 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,24 +1,38 @@
 const raining = false;
-const cold = true;
+// const cold = true;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
 
-console.log("Now you're ready to go outside!");
+// console.log("Now you're ready to go outside!");
 
-const temperature = 16;
+// const temperature = 16;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+const isCitizen = false;
+const age = 16;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 } else {
-  console.log("Short sleeves are fine.");
+  console.log("You can't vote!")
 }
 
-console.log("Now you're ready to go outside!");
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
