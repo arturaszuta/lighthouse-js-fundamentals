d0af3d9d53e92ee6adbb47aa3c520736bfc8718e
Arturas Zuta <arturaszuta@gmail.com>
2019-06-13 19:43:58 +0000

    Add greeting function

---
+++ hello.js
@@ -0,0 +1,13 @@
+// const sayHello = function () {
+//   console.log("Hello, world!");
+// }
+
+const sayHello = function(name) {
+  console.log(`Hello, ${name}!`)
+}
+
+sayHello("Tomas");
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
